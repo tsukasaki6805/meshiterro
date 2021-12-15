1627ccb7b39936aedc022ac1a80994ec10a764cb
tsukasaki6805 <kohei.tsuka46@gmail.com>
2021-12-15 14:11:53 +0000

    finish

--- Gemfile
+++ Gemfile
@@ -63,5 +63,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 
-gem "refine", require: "refine/rails", github: 'manfe/refile'
+gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+gem 'kaminari','~> 1.2.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :uses, only: [:show, :edit, :update]
 
   resources :meshiterro
 
